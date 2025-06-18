b8e90f62c8c0cac36e529bbf4a66b6812772b596
vyacheslavkalyakin <kalyakinvvv@mail.ru>
2025-06-18 11:43:26 +0300

    Краткое описание изменений

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,6 +1,8 @@
 package koschei.config;
 
+import koschei.models.Deth8;
 import koschei.models.Island2;
+import koschei.models.Needle7;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,5 +15,10 @@ public class AppConfig {
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
+    }
+
+    @Bean
+    public static Needle7 getNeedle(Deth8 deth) {
+        return new Needle7(deth);
     }
 }
